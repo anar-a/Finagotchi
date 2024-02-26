d655058b8adf217a6102b24f282ab304eb32f60a
Anar Agayev <asagf5@mail.umkc.edu>
2024-02-26 09:43:41 -0600

    Removed boilerplate and made initial file routing structure

---
+++ src/app/budget/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <div>
+      Budget Dashboard
+    </div>
+  )
+}
---
+++ src/app/pet/page.tsx
@@ -0,0 +1,7 @@
+export default function Pet() {
+  return (
+    <div>
+      Pet page
+    </div>
+  )
+}
