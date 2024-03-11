e3d896638103117cf824652d6b5d924905498eb0
SimplyJuly <91342574+SimplyJuly@users.noreply.github.com>
2024-03-11 16:56:26 -0500

    How to display an img
    You need to import the:
    - "Image" Component
    - your image from the "Public" folder
    
    Line 12 is an example of how it's used

---
+++ src/app/pages/dashboard.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import Background from '../../../public/pixelArt.jpeg'
+
+function dashboard() {
+    return (
+        <div>
+            Hellloooo
+        </div>
+    )
+}
+
+export default dashboard;
