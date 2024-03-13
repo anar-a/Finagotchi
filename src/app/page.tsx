9bba36635aeedf4f9a209238c56f7b1a8d14f995
Luke Ragan <lukeragan44@gmail.com>
2024-03-13 12:11:41 -0500

    changed sample information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import NavBar from "./NavBar"
 import Image from 'next/image'
-import Background from '../../public/couplagoats.png'
+import Background from '../../public/cat.png'
 
 
 export default function page(){
@@ -13,18 +13,15 @@ export default function page(){
     <>
     <td>
       <div className="home-content">
-       The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it 
-      continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly,
-       but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear 
-       this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to 
-       complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready,
-        start.
+       The Finagotchi is a budgeting companion that assists users with their financial needs. This pal is an animal
+       that the user takes care of via their budgeting habits. As the user gets better at budgeting the pal does better
+       physically and mentally. The worse the user does at budgeting the worse the pal does to the point of death.
       </div>
     </td>
     <>
     <td>
       <div className="home-picture">
-        home
+        
         <Image src={Background} alt="Placeholder"></Image>
       </div>
     </td>
