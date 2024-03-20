364ba47a9cdfd3e80d23588b357632f5eee206d9
SimplyJuly <91342574+SimplyJuly@users.noreply.github.com>
2024-03-19 22:22:24 -0500

    Budget DB User
    The front end is now rendering budgets that belong to the logged in user

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,8 +4,28 @@ import Carousel from '@/components/dashboardComponents/carousel';
 import Menu from '@/components/dashboardComponents/menu';
 import {Button} from '@nextui-org/react'
 import AddIcon from '../../../public/buttonIcons/add-64.png';
+import { getBudgets } from '@/actions/budget';
 
-export default function dashboard() {
+export default async function dashboard() {
+  
+  type Budget = {
+    id: bigint 
+    created_at: Date
+    name: string, 
+    target: number, 
+    user: bigint,
+    spent: number,
+  }
+
+  var budgets: Budget[];
+  const budgetsResponse = await getBudgets();
+  if (budgetsResponse) {
+    budgets = budgetsResponse;
+  } else {
+    console.error("No budget data received from the server");
+  }
+
+
   return (
     <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
       <Image 
@@ -19,10 +39,12 @@ export default function dashboard() {
           <Menu></Menu>
         </div>
         <div className="py-9 pl-28 pr-7">
-          <Carousel></Carousel>
+          {/* @ts-ignore: b.color turns into a valid property */}
+          <Carousel budgets={budgets}></Carousel>
         </div>
         <Button 
           className="bg-gradient-to-tr from-blue-600 to-purple-600 mt-15"
+          aria-label="Budget"
           style={{maxWidth: '3.75rem', maxHeight: '3.75rem', minWidth: '3.75rem', minHeight: '3.75rem'}}
           radius='full'>
           <Image src={ AddIcon } width={ 100 } alt="Add Icon for Add a Budget Button"></Image>
--- src/components/dashboardComponents/carousel.tsx
+++ src/components/dashboardComponents/carousel.tsx
@@ -1,44 +1,34 @@
 'use client';
-import { Card, Slider, Spacer } from '@nextui-org/react';
+import { Card, Slider } from '@nextui-org/react';
 import React from "react";
 import SlideShow from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const budgets = [
-  {
-    name: 'Groceries',
-    goal: 100,
-    spent: 40,
-    color: "success",
-  },
-  {
-    name: 'Squirrel Rental',
-    goal: 600,
-    spent: 540,
-    color: "warning",
-  },
-  {
-    name: 'Monthly Bills',
-    goal: 300,
-    spent: 330,
-    color: "danger",
-  },
-  {
-    name: 'Snail Racing',
-    goal: 1500,
-    spent: 2000,
-    color: "danger",
-  },
-  {
-    name: 'Cow Tattoos',
-    goal: 5,
-    spent: 3,
-    color: "warning",
-  },
-]
+type Budget = {
+  id: bigint 
+  created_at: Date
+  name: string, 
+  target: number, 
+  user: bigint,
+  spent: number,
+}
 
-function carousel() {
+interface Props {
+  budgets: Budget[];
+}
+
+function color(spent:number, target:number):string {
+  let score = (spent/target);
+  if (score<.51)
+    return 'success'
+  else if(score>1)
+    return 'danger'
+  return 'warning'
+}
+
+const carousel: React.FC<Props> = ({budgets}) => {
+  
   var settings = {
     dots: false,
     infinite: true,
@@ -49,6 +39,7 @@ function carousel() {
     autoplaySpeed: 3000,
     variableWidth: true,
   }
+
   return (
     <Card className="bg-background/60 dark:bg-default-100/50 bg-gray-100 h-28">  
       <div className="m-auto">
@@ -57,14 +48,14 @@ function carousel() {
             <div key={b.name} className='px-8'>
               <div className="bg-gradient-to-tr from-blue-500 to-purple-500 h-[80px] rounded-xl p-2 !w-32">
                 <h3 className="text-white text-center">{b.name}</h3>
-                <p className="text-xs text-white text-right">{'$'+b.spent+' / $'+b.goal}</p>
+                <p className="text-xs text-white text-right">{'$'+b.spent+' / $'+b.target}</p>
                 <Slider
                   size="md"
                   hideThumb={true}
                   // @ts-ignore: b.color turns into a valid property
-                  color={b.color}
+                  color={color(b.spent, b.target)}
                   step={0.01} 
-                  value={b.spent/b.goal}
+                  value={b.spent/b.target}
                   maxValue={1}
                   minValue={0}>
                 </Slider>
