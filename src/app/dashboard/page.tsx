989ae8adfe8f0c66fc6e5a0fa98c0c41dc45875c
SimplyJuly <91342574+SimplyJuly@users.noreply.github.com>
2024-03-21 22:44:31 -0500

    Committing changes
    Got gif to page, am working on placing the gif below the carousel, need to draw out div structure

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,6 +5,7 @@ import Menu from '@/components/dashboardComponents/menu';
 import {Button} from '@nextui-org/react'
 import AddIcon from '../../../public/buttonIcons/add-64.png';
 import { getBudgets } from '@/actions/budget';
+import Gif from '../../../public/NewPiskel.gif'
 
 export default async function dashboard() {
   
@@ -28,27 +29,44 @@ export default async function dashboard() {
 
   return (
     <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
-      <Image 
-        src={Background}
-        alt="Background Image with Pet"
-        layout="fill"
-        objectFit='cover'>
-      </Image>
+      {/* Background */}
+      <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', border: 'solid 10px black' }}>
+        <Image 
+          src={Background}
+          alt="Background Image with Pet"
+          layout="fill"
+          objectFit="cover">
+        </Image>
+      </div>
+      {/* All other components */}
       <div className = "flex h-full">
+        {/* menu */}
         <div className = "p-9 my-40">
           <Menu></Menu>
         </div>
-        <div className="py-9 pl-28 pr-7">
-          {/* @ts-ignore: b.color turns into a valid property */}
-          <Carousel budgets={budgets}></Carousel>
-        </div>
-        <Button 
-          className="bg-gradient-to-tr from-blue-600 to-purple-600 mt-15"
-          aria-label="Budget"
-          style={{maxWidth: '3.75rem', maxHeight: '3.75rem', minWidth: '3.75rem', minHeight: '3.75rem'}}
-          radius='full'>
-          <Image src={ AddIcon } width={ 100 } alt="Add Icon for Add a Budget Button"></Image>
-        </Button>
+
+        {/* Carousel */}
+          <div className="py-9 pl-28 pr-7">
+            {/* @ts-ignore: b.color turns into a valid property */}
+            <Carousel budgets={budgets}></Carousel>
+          </div>
+          
+          <div style={{ position: 'absolute', top: 0, left: 0 }} className="border-5 border-black">
+            <Image 
+            src={Gif}
+            alt="Background Image with Pet"
+            width={40}
+            height={40}
+            className="w-auto h-auto">
+            </Image>
+          </div>
+          <Button 
+            className="bg-gradient-to-tr from-blue-600 to-purple-600 mt-15"
+            aria-label="Budget"
+            style={{maxWidth: '3.75rem', maxHeight: '3.75rem', minWidth: '3.75rem', minHeight: '3.75rem'}}
+            radius='full'>
+            <Image src={ AddIcon } width={ 100 } alt="Add Icon for Add a Budget Button"></Image>
+          </Button>
       </div>
     </div>
   )
