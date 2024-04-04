52c260264acd53e9079ec7a78127c31814135940
Brandon Zubia <76754843+10brazub@users.noreply.github.com>
2024-04-04 16:02:22 -0500

    Added logout button to menu and routes to homepage

--- src/components/buttons/logout-button.tsx
+++ src/components/buttons/logout-button.tsx
@@ -1,9 +1,11 @@
+import {Button} from "@nextui-org/react";
+
 export const LogoutButton = () => {
   return (
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+    <Button className="bg-blue-500 hover:bg-blue-700 text-white whitespace-nowrap font-bold py-2 px-4">
       <a className="button__logout" href="/api/auth/logout">
         Log Out
       </a>
-    </button>
+    </Button>
   );
 };
